b1f9d3cffd8254b8d69883525fea34b017bd5a58
Kael <jalcantarabedtc@gmail.com>
2023-12-20 17:26:59 +0800

    Revert "Appointment added"
    This reverts commit a3c37a5cafcad11ca4ef1744d60e0111ed31fad6.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,32 +9,22 @@ import Dashboard from "./pages/Dashboard";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 import { UserContextProvider } from "../context/userContext";
-import AppointmentForm from "./pages/Appointment";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <main
-      style={{
-        height: "100%",
-        minHeight: "100vh",
-        
-      }}
-    >
     <UserContextProvider>
-      <Navbar className="navbar" />
+      <Navbar />
       <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/appointments" element={<AppointmentForm />} />
       </Routes>
     </UserContextProvider>
-    </main>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", require("./routes/userAuthRoutes"));
-app.use("/", require("./routes/appointmentRoutes"));
+
 
 const port = 8000;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
