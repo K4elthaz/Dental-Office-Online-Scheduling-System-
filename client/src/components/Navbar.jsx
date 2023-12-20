2434fe6883114835b46f944c99dfdf9be3e219cb
Kael <jalcantarabedtc@gmail.com>
2023-12-20 17:26:59 +0800

    Revert "Appointment added"
    This reverts commit a3c37a5cafcad11ca4ef1744d60e0111ed31fad6.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
-import { Container, Button } from "react-bootstrap";
-import "./nav.css";
-import Logo from "../assets/dental.png";
+import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 export default function Navbar() {
   return (
-    <Container fluid>
-      <div className="navigations">
-        <img src={Logo} alt="logo" className="coin" />
-        <Nav className="nav-links" activeKey="/">
-          <Link to="/" className="nav-link"><h5>Home</h5></Link>
-          <Link to="/register" className="nav-link"><h5>Register</h5></Link>
-          <Link to="/login" className="nav-link"><h5>Login</h5></Link>
-        </Nav>
-        <div className="button-container">
-          <Button>Schedule an Appointment</Button>
-        </div>
-      </div>
-      <hr></hr>
-    </Container>
-  );
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+    </nav>
+  )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -43,7 +43,7 @@ export default function Register() {
 
   return (
     <div>
-      {/* <form onSubmit={register}>
+      <form onSubmit={register}>
         <input
           type="text"
           placeholder="Enter Name"
@@ -71,55 +71,6 @@ export default function Register() {
           }
         />
         <button type="submit">Register</button>
-      </form> */}
-
-      <form class="forms" onSubmit={register}>
-        <p class="title">Register </p>
-        <p class="message">Signup now and get full access to our app. </p>
-        <label>
-        <input
-          type="text"
-          className="input"
-          value={data.name}
-          onChange={(e) => setData({ ...data, name: e.target.value })}
-        />
-          <span>Firstname</span>
-        </label>
-
-        <label>
-        <input
-          type="email"
-          className="input"
-          value={data.email}
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-        />
-          <span>Email</span>
-        </label>
-
-        <label>
-        <input
-          type="password"
-          className="input"
-          value={data.password}
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-        />
-          <span>Password</span>
-        </label>
-        <label>
-        <input
-          type="password"
-          value={data.confirmPassword}
-          className="input"
-          onChange={(e) =>
-            setData({ ...data, confirmPassword: e.target.value })
-          }
-        />
-          <span>Confirm password</span>
-        </label>
-        <button class="submit">Submit</button>
-        <p class="signin">
-          Already have an acount ? <a href="#">Signin</a>{" "}
-        </p>
       </form>
     </div>
   );
