a3c37a5cafcad11ca4ef1744d60e0111ed31fad6
Kael <jalcantarabedtc@gmail.com>
2023-12-20 16:21:19 +0800

    Appointment added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import { Container, Button } from "react-bootstrap";
+import "./nav.css";
+import Logo from "../assets/dental.png";
 
 export default function Navbar() {
   return (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-    </nav>
-  )
+    <Container fluid>
+      <div className="navigations">
+        <img src={Logo} alt="logo" className="coin" />
+        <Nav className="nav-links" activeKey="/">
+          <Link to="/" className="nav-link"><h5>Home</h5></Link>
+          <Link to="/register" className="nav-link"><h5>Register</h5></Link>
+          <Link to="/login" className="nav-link"><h5>Login</h5></Link>
+        </Nav>
+        <div className="button-container">
+          <Button>Schedule an Appointment</Button>
+        </div>
+      </div>
+      <hr></hr>
+    </Container>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import img1 from '../assets/DALLE1.png';
+import img2 from '../assets/DALLE2.png';
+import img3 from '../assets/DALLE3.png';
+
 
 export default function Home() {
+  // Define your state and variables here
+  const [headingIndex, setHeadingIndex] = useState(0);
+
+  // Define your Carousel content (image URLs) here
+  const carouselItems = [
+    { src: img1, alt: "First slide" },
+    { src: img2, alt: "Second slide" },
+    { src: img3, alt: "Third slide" },
+  ];
+
+  // Define your styles here (if needed)
+  const headingStyle = {
+    // Your heading style
+  };
+
+  const centeredHeadingStyle = {
+    // Your centered heading style
+  };
+
+  // Function to handle slide change
+  const handleSlideChange = (selectedIndex) => {
+    setHeadingIndex(selectedIndex);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Carousel
+        fade
+        controls={false}
+        indicators={false}
+        interval={4000}
+        pause={false}
+        activeIndex={headingIndex}
+        onSelect={handleSlideChange}
+      >
+        {carouselItems.map((item, index) => (
+          <Carousel.Item key={index}>
+          <img
+            className="d-block w-100 carousel-img custom-carousel-img"
+            src={item.src}
+            alt={item.alt}
+          />
+        </Carousel.Item>
+        ))}
+      </Carousel>
+      {/* <div className="backdrop" style={headingStyle}>
+        <h1 className="centered-heading" style={centeredHeadingStyle}>
+          {carouselItems[headingIndex].alt}
+        </h1>
+        <h4 className="centered-description">
+          "Protecting Your Health, Securing Your Peace of Mind - Healthcare
+          Security Solutions."
+        </h4>
+      </div> */}
+      </div>
+  );
 }
