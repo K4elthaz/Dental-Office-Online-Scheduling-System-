6ca06167efc4353459f34ce7b867197bbe39cfb8
Kael <jalcantarabedtc@gmail.com>
2023-12-21 04:41:52 +0800

    update landing

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { Button } from "react-bootstrap";
 
 function AppointmentForm() {
   const [formData, setFormData] = useState({
@@ -125,6 +126,161 @@ function AppointmentForm() {
           <button type="submit">Create Appointment</button>
         </div>
       </form>
+
+      <div>
+      <section
+        class="p-5 w-100"
+        style={{ backgroundColor: "#eee", borderRadius: ".5rem .5rem 0 0" }}
+      >
+        <div class="row">
+          <div class="col-12">
+            <div class="card text-black" style={{ borderRadius: "25px" }}>
+              <div class="card-body p-md-5">
+                <div class="row justify-content-center">
+                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p class="text-center h1 fw-bold mb-5 mt-4">Sign up</p>
+                    <form onSubmit={""}>
+                      <div className="row">
+                        <div className="col text-left">
+                          <label htmlFor="first" className="form-label">
+                            First Name
+                          </label>
+                          <input
+                            id="first"
+                            name="first"
+                            className="form-control"
+                            value={""}
+                            onChange={""}
+                            onBlur={""}
+                          />
+                          {/* {errors.first && touched.first ? (
+                            <small className="text-danger mt-1">
+                              {errors.first}
+                            </small>
+                          ) : null} */}
+                        </div>
+                        <div className="col text-left">
+                          <label htmlFor="last`" className="form-label">
+                            Last Name
+                          </label>
+                          <input
+                            id="last"
+                            name="last"
+                            className="form-control"
+                            value={""}
+                            onChange={""}
+                            onBlur={""}
+                          />
+                          {/* {errors.last && touched.last ? (
+                            <small className="text-danger mt-1">
+                              {errors.last}
+                            </small>
+                          ) : null} */}
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col text-left">
+                          <label htmlFor="first" className="form-label">
+                            Email
+                          </label>
+                          <input
+                            id="email"
+                            name="email"
+                            className="form-control"
+                            value={""}
+                            onChange={""}
+                            onBlur={""}
+                          />
+                          {/* {errors.email && touched.email ? (
+                            <small className="text-danger mt-1">
+                              {errors.email}
+                            </small>
+                          ) : null} */}
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col text-left">
+                          <label htmlFor="first" className="form-label">
+                            Password
+                          </label>
+                          <input
+                            id="password"
+                            name="password"
+                            className="form-control"
+                            value={""}
+                            onChange={""}
+                            onBlur={""}
+                            type="password"
+                          />
+                          {/* {errors.password && touched.password ? (
+                            <small className="text-danger mt-1">
+                              {errors.password}
+                            </small>
+                          ) : null} */}
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col text-left">
+                          <label htmlFor="first" className="form-label">
+                            Confirm Password
+                          </label>
+                          <input
+                            id="repassword"
+                            name="repassword"
+                            className="form-control"
+                            value={""}
+                            onChange={""}
+                            onBlur={""}
+                            type="password"
+                          />
+                          {/* {errors.repassword && touched.repassword ? (
+                            <small className="text-danger mt-1">
+                              {errors.repassword}
+                            </small>
+                          ) : null} */}
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col text-right actionButtons">
+                          <Button
+                            variant="secondary"
+                            size="sm"
+                            onClick={""}
+                          >
+                            Clear
+                          </Button>
+
+                          <Button
+                            variant="primary"
+                            size="sm"
+                            onClick={""}
+                          >
+                            Register
+                          </Button>
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <br />
+                        <div className="col text-right">
+                          Already have an account? <a href="/">Sign in</a>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
     </div>
   );
 }
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -9,48 +9,86 @@ import {
   Card,
 } from "react-bootstrap";
 import "./test.css";
-const test = () => {
-  return (
-    <div>
-      {/* Header Section */}
-      <Navbar bg="light" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Brand href="#home">
-            dental<span>Clinic.</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="#home">home</Nav.Link>
-              <Nav.Link href="#about">about</Nav.Link>
-              <Nav.Link href="#services">services</Nav.Link>
-              <Nav.Link href="#reviews">reviews</Nav.Link>
-              <Nav.Link href="#contact">contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          <Button href="#contact" className="link-btn">
-            make appointment
-          </Button>
-        </Container>
-      </Navbar>
+import img1 from "../assets/img8.png";
+import img2 from "../assets/3.png";
+import img3 from "../assets/4.png";
+import img4 from "../assets/5.png";
+import img5 from "../assets/6.png";
+import img6 from "../assets/logo2.png";
+import svg1 from "../assets/icon-1.svg";
+import svg2 from "../assets/icon-2.svg";
+import svg3 from "../assets/icon-3.svg";
+import svg4 from "../assets/icon-4.svg";
 
-      {/* Home Section */}
-      <section className="home" id="home">
-        <Container>
-          <Row className="min-vh-100 align-items-center">
-            <Col className="text-center text-md-left">
-              <h3>Allow us to make your smile brighter.</h3>
-              <p>
+const test = () => {
+  
+  return (
+    <div className="special_page"  style={{ fontSize: "62.5%" }}>
+      <section className="home min-vh-100" id="home" style={{ fontSize: "5px" }}>
+        <div className="container">
+          <Row>
+            <Col xs lg="1" className="d-flex align-items-center">
+              <img src={img6} className="slogan" alt="" />
+            </Col>
+            <Col md="auto" className="d-flex align-items-center">
+              <div className="mt-4 text-green">
+                <h2>Genuine Family Dental Clinic</h2>
+                <h3>You're Smile is My Happiness</h3>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+
+          <div className="row mt-5 align-items-center">
+            <div className="content text-center text-md-left">
+              <h3 className="text-green">
+                Where Your Smile Finds Its Sparkle!
+              </h3>
+              <p className="text-green">
                 DentalClinic Can Help You Get the Smile You've Always Wanted. We
                 offer cosmetic dentistry, root canal therapy, cavity
                 inspections, and more.
               </p>
-              <Button href="#contact" className="link-btn">
+              <a href="#contact" className="link-btn">
                 make appointment
-              </Button>
-            </Col>
-          </Row>
-        </Container>
+              </a>
+              <div className="image-container d-flex justify-content-center align-items-center">
+                <div className="image-text-container">
+                  <div className="image-wrapper box">
+                    <img src={img2} className="image20" alt="" />
+                  </div>
+                  <p className="text-green">
+                    Specialized <br /> Services
+                  </p>
+                </div>
+                <div className="image-text-container">
+                  <div className="image-wrapper box">
+                    <img src={img3} className="image20" alt="" />
+                  </div>
+                  <p className="text-green">
+                    Emergency <br /> Care
+                  </p>
+                </div>
+                <div className="image-text-container">
+                  <div className="image-wrapper box">
+                    <img src={img4} className="image20" alt="" />
+                  </div>
+                  <p className="text-green">
+                    Teeth <br /> Services
+                  </p>
+                </div>
+                <div className="image-text-container">
+                  <div className="image-wrapper box">
+                    <img src={img5} className="image20" alt="" />
+                  </div>
+                  <p className="text-green">
+                    Orthodontics <br /> Services
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* About Section */}
@@ -58,16 +96,12 @@ const test = () => {
         <Container>
           <Row className="align-items-center">
             <Col md={6} className="image">
-              <img
-                src="images/about-img.jpg"
-                className="w-100 mb-5 mb-md-0"
-                alt=""
-              />
+              <img src={img1} className="w-100 mb-5 mb-md-0" alt="" />
             </Col>
-            <Col md={6} className="content">
-              <span>about us</span>
-              <h3>Genuine Family Healthcare</h3>
-              <p>
+            <Col md={6} className="content" style={{ padding: "50px" }}>
+              <span className="text-green">about us</span>
+              <h3 className="text-green">Genuine Family Dental Clinic</h3>
+              <p className="text-green">
                 DentalClinic helps you achieve the quintessentially oriented
                 smile you have always craved. Our product gets the job done
                 without making you go through any hassle or discomfort.
@@ -82,115 +116,117 @@ const test = () => {
 
       {/* Services Section */}
       <section className="services" id="services">
-        <Container>
-          <h1 className="heading">our services</h1>
-          <Row className="box-container">
-            <Col md={4} className="box">
-              <img src="images/icon-1.svg" alt="" />
-              <h3>Alignment specialist</h3>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/icon-2.svg" alt="" />
-              <h3>Cosmetic dentistry</h3>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/icon-3.svg" alt="" />
-              <h3>Oral hygiene experts</h3>
-            </Col>
-            {/* Add more service boxes here */}
-          </Row>
-        </Container>
+        <h1 className="heading">our services</h1>
+
+        <div className="box-container container">
+          <div className="box">
+            <img src={svg1} alt="" />
+            <h3 className="text-green">Alignment specialist</h3>
+          </div>
+
+          <div className="box">
+            <img src={svg2} alt="" />
+            <h3 className="text-green">Cosmetic dentistry</h3>
+          </div>
+
+          <div className="box">
+            <img src={svg3} alt="" />
+            <h3 className="text-green">Oral hygiene experts</h3>
+          </div>
+
+          <div className="box">
+            <img src={svg4} alt="" />
+            <h3 className="text-green">Root canal specialist</h3>
+          </div>
+        </div>
       </section>
 
       {/* Process Section */}
-      <section className="process">
-        <Container>
-          <h1 className="heading">work process</h1>
-          <Row className="box-container">
-            <Col md={4} className="box">
-              <img src="images/process-1.png" alt="" />
-              <h3>Cosmetic Dentistry</h3>
-              <p>
-                Cosmetic dentistry includes teeth whitening, dental implants,
-                dental crowns, and teeth shaping.
-              </p>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/process-2.png" alt="" />
-              <h3>Pediatric Dentistry</h3>
-              <p>
-                Pediatric dentistry includes stainless steel crowns,
-                tooth-colored fillings, dental cleanings, and cavities.
-              </p>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/process-3.png" alt="" />
-              <h3>Dental Implants</h3>
-              <p>
-                Dental implants are artificial tooth roots that are surgically
-                placed into the jawbone.
-              </p>
-            </Col>
-          </Row>
-        </Container>
+      <section class="process">
+        <h1 class="heading">work process</h1>
+
+        <div class="box-container container">
+          <div class="box">
+            <img src="images/process-1.png" alt="" />
+            <h3>Cosmetic Dentistry</h3>
+            <p>
+              Cosmetic dentistry includes teeth whitening, dental implants,
+              dental crowns, and teeth shaping.
+            </p>
+          </div>
+
+          <div class="box">
+            <img src="images/process-2.png" alt="" />
+            <h3>Pediatric Dentistry</h3>
+            <p>
+              Padiatric dentistry include stainless steel crowns, tooth-colored
+              fillings, dental cleanings, and cavities.
+            </p>
+          </div>
+
+          <div class="box">
+            <img src="images/process-3.png" alt="" />
+            <h3>Dental Implants</h3>
+            <p>
+              Dental implants are artificial tooth roots that are surgically
+              placed into the jawbone.
+            </p>
+          </div>
+        </div>
       </section>
 
       {/* Reviews Section */}
-      <section className="reviews" id="reviews">
-        <Container>
-          <h1 className="heading">Our Clients</h1>
-          <Row className="box-container">
-            <Col md={4} className="box">
-              <img src="images/pic-1.png" alt="" />
-              <p>
-                I couldn’t believe that it was so affordable compared to the
-                alternatives available in the market.
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
-              <h3>Alan Harris</h3>
-              <span>Manager</span>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/pic-2.png" alt="" />
-              <p>
-                Earlier I used to hide my smile and now I can’t stop smiling.
-                Flexalign aligners changed my life &amp; smile completely.
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
-              <h3>Sophie Johnson</h3>
-              <span>Assistant Manager</span>
-            </Col>
-            <Col md={4} className="box">
-              <img src="images/pic-3.png" alt="" />
-              <p>
-                Great experience with DentalClinic aligners. I would recommend
-                this place for they have the best quality service.
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
-              </div>
-              <h3>James Williams</h3>
-              <span>CEO</span>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+      <section class="reviews" id="reviews">
+
+<h1 class="heading"> Our Clients </h1>
+
+<div class="box-container container">
+
+   <div class="box">
+      <img src="images/pic-1.png" alt=""/>
+      <p> I couldn’t believe that it was so afordable compared to the alternatives available in the market.</p>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+      </div>
+      <h3>Alan Harris</h3>
+      <span>Manager</span>
+   </div>
+
+   <div class="box">
+      <img src="images/pic-2.png" alt=""/>
+      <p>Earlier I used to hide my smile and now I can’t stop smiling. Flexalign aligners changed my life &amp; smile completely.</p>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+      </div>
+      <h3>Sophie Johnson</h3>
+      <span>Assistant Manager</span>
+   </div>
+
+   <div class="box">
+      <img src="images/pic-3.png" alt=""/>
+      <p>Great experience with DentalClinic aligners. I would recommend this place for they have the best quality service</p>
+      <div class="stars">
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star"></i>
+         <i class="fas fa-star-half-alt"></i>
+      </div>
+      <h3>James Williams</h3>
+      <span>CEO</span>
+   </div>
+
+</div>
+
+</section>
 
       {/* Contact Section */}
       <section className="contact" id="contact">
@@ -234,22 +270,22 @@ const test = () => {
       <section className="footer">
         <Container>
           <Row>
-            <Col md={3} className="box">
+            <Col md={3} className="">
               <i className="fas fa-phone"></i>
               <h3>Phone Number</h3>
               <p>+123-456-7890</p>
             </Col>
-            <Col md={3} className="box">
+            <Col md={3} className="">
               <i className="fas fa-map-marker-alt"></i>
               <h3>Our Address</h3>
               <p>Mumbai, India - 400054</p>
             </Col>
-            <Col md={3} className="box">
+            <Col md={3} className="">
               <i className="fas fa-clock"></i>
               <h3>Opening Hours</h3>
               <p>10:00am to 07:00pm</p>
             </Col>
-            <Col md={3} className="box">
+            <Col md={3} className="">
               <i className="fas fa-envelope"></i>
               <h3>Email Address</h3>
               <p>valentine@gmail.com</p>
@@ -257,7 +293,7 @@ const test = () => {
           </Row>
         </Container>
         <div className="credit">
-          &copy;2022 {new Date().getFullYear()} <span>DentalClinic</span> All
+          &copy;2023 {new Date().getFullYear()} <span>DentalClinic</span> All
           Reserved Rights
         </div>
       </section>
